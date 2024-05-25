c6aeb2bc021455e681b42d9975eaba0cc9a22b74
Mahedi205339 <mahedi205339@gmail.com>
2024-05-25 13:18:13 +0600

    creaded navicon profile routing and navigation part

--- src/components/Navicons.tsx
+++ src/components/Navicons.tsx
@@ -1,9 +1,51 @@
-import React from 'react';
+"use client"
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
 
 const NavIcons = () => {
+
+    const [isProfileOpen, setIsProfileOpen] = useState(false)
+    const [isCartOpen, setIsCartOpen] = useState(false);
+
+    const router = useRouter();
+
+    // for now 
+    const isLoggedIn = false;
+
+    const handleProfile = () => {
+        if (!isLoggedIn) {
+            router.push("/login")
+        }
+        setIsProfileOpen((prev) => !prev)
+    }
+
     return (
-        <div>
-            Navicons
+        <div className='flex items-center gap-4 xl:gap-6 relative'>
+            <Image src="/profile.png"
+                width={22}
+                height={22}
+                alt=''
+                className='cursor-pointer'
+                onClick={handleProfile}
+            />
+            {isProfileOpen &&
+                <div className='absolute p-4 rounded-md top-1/2 text-sm left-0 shadow-neutral-200 shadow-lg z-20'>
+                    <Link href="/">profile</Link>
+                    <div className='mt-2 cursor-pointer'>Logout</div>
+
+                </div>
+            }
+            <Image src="/notification.png" width={22} height={22} alt='' className='cursor-pointer' />
+            <Image src="/cart.png" width={22} height={22} alt='' className='cursor-pointer'
+                onClick={() => setIsCartOpen((prev) => !prev)}
+            />
+            {
+                isCartOpen && (
+                    
+                )
+            }
         </div>
     );
 };
