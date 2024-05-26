26fdb7914519783455c5143769dd64ef2b719826
Mahedi205339 <mahedi205339@gmail.com>
2024-05-26 20:23:57 +0600

    added slider comp and data

---
+++ src/components/Slider.tsx
@@ -0,0 +1,58 @@
+"use client"
+import React from 'react';
+
+const Slider = () => {
+
+    const slides = [
+        {
+          id: 1,
+          title: "Summer Sale Collections",
+          description: "Sale! Up to 50% off!",
+          img: "https://i.ibb.co/3BSB5Qs/khaled-ghareeb-Ny-Pn9up-7o-unsplash.jpg",
+          url: "/",
+          bg: "bg-gradient-to-r from-yellow-50 to-pink-50",
+        },
+        {
+          id: 2,
+          title: "Winter Sale Collections",
+          description: "Sale! Up to 50% off!",
+          img: "https://i.ibb.co/PQpBQvP/adriano-goncalves-Ie-HLtt-Crs-XE-unsplash.jpg",
+          url: "/",
+          bg: "bg-gradient-to-r from-pink-50 to-blue-50",
+        },
+        {
+          id: 3,
+          title: "Spring Sale Collections",
+          description: "Sale! Up to 50% off!",
+          img: "https://i.ibb.co/dB5wcVv/yasu-shots-y6h-Mz1p-JC0-unsplash.jpg",
+          url: "/",
+          bg: "bg-gradient-to-r from-blue-50 to-yellow-50",
+        },
+        ,
+        {
+          id: 3,
+          title: "Spring Sale Collections",
+          description: "Sale! Up to 50% off!",
+          img: "https://i.ibb.co/LhhrcDr/dom-hill-nim-El-Tc-TNy-Y-unsplash.jpg",
+          url: "/",
+          bg: "bg-gradient-to-r from-blue-50 to-yellow-50",
+        },
+        ,
+        {
+          id: 3,
+          title: "Spring Sale Collections",
+          description: "Sale! Up to 50% off!",
+          img: "https://i.ibb.co/wKrp3S5/mahdi-bafande-HOTWXfb-II6k-unsplash.jpg",
+          url: "/",
+          bg: "bg-gradient-to-r from-blue-50 to-yellow-50",
+        },
+      ];
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Slider;
