6b4f6ef1291ace504455bc9f3cdbf705d7037bf3
Mahedi205339 <mahedi205339@gmail.com>
2024-05-28 19:44:41 +0600

    added product lsit full design

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -13,13 +13,20 @@ const ProductList = () => {
                         fill
                         sizes='25vw'
                         className='absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500'
-                        />
+                    />
                     <Image
                         src="https://i.ibb.co/7Q5Y5nj/adriano-goncalves-Ie-HLtt-Crs-XE-unsplash.jpg"
                         alt=''
                         fill
                         sizes='25vw' />
                 </div>
+
+                <div className='flex justify-between'>
+                    <span className='font-medium '>Product Name</span>
+                    <span className='font-semibold'>$45</span>
+                </div>
+                <div className='text-sm text-gray-500 '>My description</div>
+                <button className='rounded-2xl ring-1 ring-fashion text-fashion hover:bg-fashion hover:text-white w-max px-4 py-2'>Add to Cart</button>
             </Link>
         </div>
     );
