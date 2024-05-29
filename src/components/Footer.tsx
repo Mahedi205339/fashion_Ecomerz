772ab42611578659211ab1acc242da54b42c604e
Mahedi205339 <mahedi205339@gmail.com>
2024-05-29 15:56:25 +0600

    added some extra changes in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -90,10 +90,12 @@ const Footer = () => {
             <div className="flex flex-col md:flex-row items-center justify-between gap-8 mt-16">
                 <div className="">© 2024 Lama Shop</div>
                 <div className="flex flex-col gap-8 md:flex-row">
-                    <div className="">
+                    <div className="flex flex-row gap-5">
                         <span className="text-gray-500 mr-4">Language</span>
                         <span className="font-medium">United States | English</span>
+                        <span className="font-medium">Bangladesh | Bangla</span>
                     </div>
+                   
                     <div className="">
                         <span className="text-gray-500 mr-4">Currency</span>
                         <span className="font-medium">$ USD</span>
