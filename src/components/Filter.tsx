48f71595178a0d1425b8fa0443bc37f1776b96cb
Mahedi205339 <mahedi205339@gmail.com>
2024-05-30 19:55:53 +0600

    added filter component and added selection section

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Filter = () => {
     return (
-        <div>
-            
+        <div className='mt-12 flex justify-between'>
+            <div>
+                <select name="type" id="" className='py-2 rounded-2xl text-xs font-medium bg-white'>
+                    <option>Type</option>
+                    <option value="digital">Digital</option>
+                    <option value="physical">Physical</option>
+                </select>
+                <input type="text" name='min' placeholder='min price' />
+            </div>
+            <div>
+
+            </div>
         </div>
     );
 };
