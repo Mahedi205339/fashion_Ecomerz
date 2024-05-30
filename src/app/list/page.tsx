0f86b8345f331911df638a020ef29ad79c800601
Mahedi205339 <mahedi205339@gmail.com>
2024-05-30 19:48:10 +0600

    added ful page-list banner

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,21 +1,33 @@
+import Filter from '@/components/Filter';
+import ProductList from '@/components/ProductList';
 import Image from 'next/image';
 import React from 'react';
 
 const ListPage = () => {
     return (
         <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
-            <div>
-                <div>
-                    <h1>Grab up to 50% off on
+            <div className='bg-pink-50 p-4 flex justify-between h-64'>
+                <div className='w-2/3 flex flex-col items-center justify-center gap-8'>
+                    <h1 className='text-4xl font-semibold leading-[48px] text-gray-700'>
+                        Grab up to 50% off on
+                        <br />
                         Selected Products
 
                     </h1>
+                    <button className='rounded-3xl bg-fashion text-white w-max py-3 px-5 text-sm'>Buy Now</button>
 
                 </div>
                 <div className='relative w-1/3'>
-                <Image src="/woman.png" alt='' fill className='overscroll-contain'/>
+                    <Image src="/woman.png" alt='' fill className='overscroll-contain' />
                 </div>
             </div>
+            {/* Filter */}
+            <Filter/>
+            {/* Product  */}
+            <h1 className='mt-12 font-semibold text-xl'>
+                Shoes For You!
+            </h1>
+             <ProductList/>
 
         </div>
     );
