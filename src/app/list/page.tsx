2a89ea63e6a5deda79949e1d21bc2cb4e2e2a4ef
Mahedi205339 <mahedi205339@gmail.com>
2024-05-30 19:34:52 +0600

    added list page layout

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,9 +1,22 @@
+import Image from 'next/image';
 import React from 'react';
 
 const ListPage = () => {
     return (
-        <div>
-            List page
+        <div className='px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative'>
+            <div>
+                <div>
+                    <h1>Grab up to 50% off on
+                        Selected Products
+
+                    </h1>
+
+                </div>
+                <div className='relative w-1/3'>
+                <Image src="/woman.png" alt='' fill className='overscroll-contain'/>
+                </div>
+            </div>
+
         </div>
     );
 };
